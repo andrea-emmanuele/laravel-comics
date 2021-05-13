ba2a7d46f94a7b45f2da8719b39d2b018140a566
andrea <git@andreaemmanuele.it>
2021-05-13 10:52:07 +0200

    Layout completed

--- resources/views/layouts/footer/footer.blade.php
+++ resources/views/layouts/footer/footer.blade.php
@@ -1,3 +1,131 @@
  <footer>
-     footer
+     <div class="content">
+         <section id="main">
+             <div class="inner-content">
+                 <div class="col">
+                     <div class="item-group">
+                         <div class="item">
+                             <h1>dc comics</h1>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                     </div>
+                     <div class="item-group">
+                         <div class="item">
+                             <h1>shop</h1>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col">
+                     <div class="item-group">
+                         <div class="item">
+                             <h1>dc</h1>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col">
+                     <div class="item-group">
+                         <div class="item">
+                             <h1>sites</h1>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                         <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                        <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                        <div class="item">
+                             <a href="#">Lorem ipsum.</a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="inner-content">
+                 <div class="item">
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At inventore minima optio soluta vel vitae?<br><a href="#">Cookie Settings</a></p>
+                 </div>
+             </div>
+         </section>
+     </div>
+     <section id="pie">
+         <div class="content">
+             <div class="inner-content">
+                 <a href="#" class="sign-up-btn">sign-up now!</a>
+             </div>
+             <div class="inner-content">
+                 <div class="item-group">
+                     <span class="follow-us">follow us</span>
+                     <a href="#" class="icon"><img src="images/footer-facebook.png" alt="facebook"></a>
+                     <a href="#" class="icon"><img src="images/footer-twitter.png" alt="facebook"></a>
+                     <a href="#" class="icon"><img src="images/footer-youtube.png" alt="facebook"></a>
+                     <a href="#" class="icon"><img src="images/footer-pinterest.png" alt="facebook"></a>
+                     <a href="#" class="icon"><img src="images/footer-periscope.png" alt="facebook"></a>
+                 </div>
+             </div>
+         </div>
+     </section>
  </footer>
