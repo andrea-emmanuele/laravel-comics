ece0337031a758a70a54299674596ad8fe8735d0
andrea <git@andreaemmanuele.it>
2021-05-14 18:35:48 +0200

    refactoring

--- resources/views/layouts/header/nav.blade.php
+++ resources/views/layouts/header/nav.blade.php
@@ -19,16 +19,16 @@
                 <a href="/"><img src="../images/dc-logo.png" alt="brand"></a>
             </div>
             <nav>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item active"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item"><a href="#" class="secondary">Lorem</a></div>
-                <div class="item dropdown"><span class="secondary">Lorem</span></div>
+                <div class="item"><a href="#" class="secondary">characters</a></div>
+                <div class="item active"><a href="#" class="secondary">comics</a></div>
+                <div class="item"><a href="#" class="secondary">movies</a></div>
+                <div class="item"><a href="#" class="secondary">tv</a></div>
+                <div class="item"><a href="#" class="secondary">games</a></div>
+                <div class="item"><a href="#" class="secondary">collectibles</a></div>
+                <div class="item"><a href="#" class="secondary">videos</a></div>
+                <div class="item"><a href="#" class="secondary">fans</a></div>
+                <div class="item"><a href="#" class="secondary">news</a></div>
+                <div class="item dropdown"><span class="secondary">shop</span></div>
                 <div class="underline"></div>
             </nav>
             <div class="search-box">
